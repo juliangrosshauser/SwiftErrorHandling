616cf6cc772a0ec3acb1abc023a0099343cb7a64
Julian Grosshauser <julian@juliangrosshauser.com>
2015-08-03 22:18:18 +0200

    Add "Old Approach" section

--- SwiftErrorHandling.playground/Contents.swift
+++ SwiftErrorHandling.playground/Contents.swift
@@ -1,1 +1,26 @@
+/*:
+# Swift Error Handling
 
+## Old Approach
+
+Let's take a look at the following Objective-C method:
+
+    - (void)cacheData:(NSData *)data error:(NSError **)error completionHandler:(BOOL (^)(void))completionHandler;
+
+It uses an in-out parameter called `error` to signal any errors during execution.
+
+You would use it like this:
+
+    NSError *error = nil;
+
+    [self cacheData:data error:&error completionHandler:completionHandler];
+
+    if (error != nil) {
+        // Handle error
+    }
+
+Looks okay, right? Why would they change this?
+
+* You can just pass `NULL` as `error` parameter and completely ignore the error
+* Even if you use the `error` in-out parameter you could forget to check it after calling the method
+*/
